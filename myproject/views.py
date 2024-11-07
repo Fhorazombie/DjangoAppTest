25ecec2fcbf1c43557511297b6c565133499d5aa
Yoshua Palacios <yoshua.palacios@xaldigital.com>
2024-11-07 15:12:58 -0600

    remove urls

--- myproject/views.py
+++ myproject/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def menu_view(request):
     return render(request, 'menu.html', {
         'modules': [
-            {'name': 'Module 1', 'url': '/module1/'},
+            #{'name': 'Module 1', 'url': '/module1/'},
             {'name': 'Module 2', 'url': '/module2/'},
         ]
     })
